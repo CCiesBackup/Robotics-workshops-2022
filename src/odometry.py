126fa9b8195f7adf4801ea6af9157bfa6c6323e5
root <maximilian_lutz.mueller@mailbox.tu-dresden.de>
2022-08-18 14:43:56 +0200

    WIP

--- src/odometry.py
+++ src/odometry.py
@@ -1,11 +1,37 @@
 # !/usr/bin/env python3
-
-
+"""import ev3dev.ev3 as ev3
+>>> cs = ev3.ColorSensor()
+>>> cs.mode = 'RGB-RAW'
+>>> cs.bin_data("hhh")
+(354, 415, 543)
+"""
 class Odometry:
+    int[] = []
     def __init__(self):
         """
         Initializes odometry module
         """
 
-        # YOUR CODE FOLLOWS (remove pass, please!)
-        pass
+    def direction(self):
+        alpha = 0
+    
+    
+    def black_portion(self):
+        
+        
+    
+    def linefollowing(self):
+        Kp = 
+        offset = 
+        Tp = 300
+        cs = ev3.ColorSensor()
+        cs.mode = "COL-COLOR"
+        if cs.value() == 2 or cs.value == 5: 
+            
+        else: 
+        cs.mode = "RGB-RAW"
+            while True:
+                LightValue = cs.bin_data("hhh")
+                error = LightValue - offset
+            
+        
