2cbcc5e116149c90e01b2d72947316ac51c76f51
Cezary Cieśliński <cezary.cieslinski@mailbox.tu-dresden.de>
2022-08-18 14:05:35 +0200

    get_paths and add_path implemented

--- src/planet.py
+++ src/planet.py
@@ -25,65 +25,36 @@ Value:  -1 if blocked path
 
 
 class Planet:
-    """
-    Contains the representation of the map and provides certain functions to manipulate or extend
-    it according to the specifications
-    """
+    __paths = Dict[Tuple[int, int], Dict[Direction, Tuple[Tuple[int, int], Direction, Weight]]]
 
     def __init__(self):
-        """ Initializes the data structure """
-        self.paths = {}
+        self.__paths = {}
 
-    def add_path(self, start: Tuple[Tuple[int, int], Direction], target: Tuple[Tuple[int, int], Direction],
+    def add_path(self,
+                 start: Tuple[Tuple[int, int], Direction],
+                 target: Tuple[Tuple[int, int], Direction],
                  weight: int):
-        """
-         Adds a bidirectional path defined between the start and end coordinates to the map and assigns the weight to it
+        start_value_dictionary = self.__paths[start[0]]
+        start_value_dictionary[start[1]] = Tuple[target[0], target[1], weight]
 
-        Example:
-            add_path(((0, 3), Direction.NORTH), ((0, 3), Direction.WEST), 1)
-        :param start: 2-Tuple
-        :param target:  2-Tuple
-        :param weight: Integer
-        :return: void
-        """
 
-        # YOUR CODE FOLLOWS (remove pass, please!)
-        pass
 
-    def get_paths(self) -> Dict[Tuple[int, int], Dict[Direction, Tuple[Tuple[int, int], Direction, Weight]]]:
-        """
-        Returns all paths
 
-        Example:
-            {
-                (0, 3): {
-                    Direction.NORTH: ((0, 3), Direction.WEST, 1),
-                    Direction.EAST: ((1, 3), Direction.WEST, 2),
-                    Direction.WEST: ((0, 3), Direction.NORTH, 1)
-                },
-                (1, 3): {
-                    Direction.WEST: ((0, 3), Direction.EAST, 2),
-                    ...
-                },
-                ...
-            }
-        :return: Dict
-        """
 
-        # YOUR CODE FOLLOWS (remove pass, please!)
-        pass
+
+    def get_paths(self) -> \
+            Dict[Tuple[int, int],
+                 Dict[Direction,
+                      Tuple[Tuple[int, int], Direction, Weight]
+                 ]
+            ]:
+
+        return self.__paths
+
+
 
     def shortest_path(self, start: Tuple[int, int], target: Tuple[int, int]) -> Optional[List[Tuple[Tuple[int, int], Direction]]]:
-        """
-        Returns a shortest path between two nodes
 
-        Examples:
-            shortest_path((0,0), (2,2)) returns: [((0, 0), Direction.EAST), ((1, 0), Direction.NORTH)]
-            shortest_path((0,0), (1,2)) returns: None
-        :param start: 2-Tuple
-        :param target: 2-Tuple
-        :return: None, List[] or List[Tuple[Tuple[int, int], Direction]]
-        """
 
-        # YOUR CODE FOLLOWS (remove pass, please!)
+
         pass
