aea5f6b671068da99d9986c427fd6da3d818dad6
Nicolas Apel <nicolas.apel@mailbox.tu-dresden.de>
2022-08-19 13:50:00 +0200

    implement followLine Function

--- src/odometry.py
+++ src/odometry.py
@@ -5,8 +5,19 @@
 >>> cs.bin_data("hhh")
 (354, 415, 543)
 """
+
+# Navigation, Koordinaten, Richtung der Strecken
+# Navigation durch externe Klassen
+import ev3dev.ev3 as ev3
+from services.farbsensor import Farbsensor
+from services.motor import LeftEdge
+
+
 class Odometry:
-    int[] = []
+    colorArray = []
+    strategy = LeftEdge
+    farbsensor = Farbsensor
+    
     def __init__(self):
         """
         Initializes odometry module
@@ -15,23 +26,27 @@ class Odometry:
     def direction(self):
         alpha = 0
     
-    
-    def black_portion(self):
-        
-        
-    
     def linefollowing(self):
-        Kp = 
-        offset = 
-        Tp = 300
+        left = ev3.LargeMotor("outA")
+        right = ev3.LargeMotor("outD")
+        
+        left.reset()
+        right.reset()
+        
+        left.stop_action = "brake"
+        right.stop_action = "brake"
+        
+        left.command = "run-forever"
+        right.command = "run-forever"
+        
+        kp = 300
+        offset = 0.5
         cs = ev3.ColorSensor()
         cs.mode = "COL-COLOR"
-        if cs.value() == 2 or cs.value == 5: 
-            
-        else: 
         cs.mode = "RGB-RAW"
-            while True:
-                LightValue = cs.bin_data("hhh")
-                error = LightValue - offset
-            
-        
+        while True:
+            print(cs.bin_data("hhh"))
+            LightValue = self.farbsensor.getBlackWhitePortion(cs.bin_data("hhh")[0], cs.bin_data("hhh")[1], cs.bin_data("hhh")[2])
+            error = LightValue - offset
+            print(f'Error: {error}')
+            self.strategy.driving(error * kp, left, right)
